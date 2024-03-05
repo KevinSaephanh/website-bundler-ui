c4697ff57d46bca88b00702d7446371b06e05180
Kevin Saephanh <Kevin.Saephanh@gmail.com>
2024-03-05 14:01:56 -0600

    Add google oauth

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,3 +1,31 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  sendResponse({ success: true, message: 'Hello World' });
+function getOAuthUrl() {
+  const authUrl = 'https://accounts.google.com/o/oauth2/auth?';
+  const clientId = process.env['GOOGLE_CLIENT_ID']!;
+  const redirectUri = chrome.identity.getRedirectURL();
+  return `${authUrl}client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&scope=email`;
+}
+
+function checkAuth() {
+  return (
+    chrome.identity.getAuthToken({ interactive: false }), (token) => !!token
+  );
+}
+
+chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
+  if (request.message === 'login') {
+    // Check if user is already authenticated
+    const isAuth = checkAuth();
+    if (isAuth) return;
+
+    // Redirect to Google OAuth
+    chrome.identity.launchWebAuthFlow(
+      {
+        url: getOAuthUrl(),
+        interactive: true,
+      },
+      function (redirect_uri) {
+        sendResponse(`Success, redirecting to: ${redirect_uri}`);
+      }
+    );
+  }
 });
